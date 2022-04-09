11e39e7f1a292eb9dbf9f790d08f40c1c7647184
Gyanendrachauhan <101542263+Gyanendrachauhan@users.noreply.github.com>
2022-04-09 14:10:27 +0530

    Add files via upload

---
+++ github python.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **MY PYTHON NOTE BOOK IN GITHUB(NOT IN IBM STUDIO BECAUSE THERE IS TECCHNICAL ERROR)**
+
+# **NAME-GYANENDRA SINGH**
+
+# *it is very intresting to get the insights from the data*
+# 
+# 
+
+# # the following code find the simple intrest.
+# 
+
+# In[6]:
+
+
+import math
+def simple_intrest(p,t,r):
+    print ("the principle is ", p)
+    print ("the time is ", t)
+    print ("the rate is", r)
+    
+    si=(p*t*r)/100
+    print ("the simple intrest is", si )
+    return si
+simple_intrest(7000,7,7)
+    
+
+
+# In[ ]:
+
+
+
+
